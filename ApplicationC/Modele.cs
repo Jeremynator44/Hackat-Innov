c97aa0e216e78fc7e140722ebf1b4940c3592c10
Jérémy Loubeyre <jeremy.loubeyre44@gmail.com>
2024-09-06 14:56:48 +0200

    Ajoutez des fichiers projet.

---
+++ ApplicationC/Modele.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationC.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationC
+{
+    public static class Modele
+    {
+        private static Ap3LesProfsContext monModel;
+
+        public static Ap3LesProfsContext MonModel { get => monModel; set => monModel = value; }
+
+        public static void init()
+        {
+            monModel = new Ap3LesProfsContext();
+        }
+    }
+}
